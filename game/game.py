1c60ddd63d567029ef27266e2bdf9d7d9e8a89ac
Ezike Ebuka <yerimyah1@gmail.com>
2022-03-30 22:42:46 +0100

    Modified game.py and boardstate.py

--- game/game.py
+++ game/game.py
@@ -10,6 +10,7 @@ class Game:
     __state: deque[BoardState] = deque()
     __memory: int = Settings.UNDO_MEM
     __ai: AI = AI()
+    __human_won: bool
 
     def __init__(self):
         self.__state.append(BoardState())
@@ -20,10 +21,10 @@ class Game:
 
     def playerMove(self, from_pos: int, dx: int, dy: int) -> MoveFeedBack:
         to_pos: int = from_pos + dx + BoardState.SIDE_LENGTH * dy
-        if to_pos > self.getState().state.length:  # change
+        if to_pos > len(self.get_state().state):
             return MoveFeedBack.NOT_ON_BOARD
 
-        jump_successors: list[BoardState] = self.state_peek().get_successors(True)
+        jump_successors: list[BoardState] = self.state_peek().get_successors_jump(True)
         can_jump: bool = len(jump_successors) > 0
         if can_jump:
             for succ in jump_successors:
@@ -39,11 +40,11 @@ class Game:
         if self.get_state().state[to_pos] is not None:
             return MoveFeedBack.NO_FREE_SPACE
 
-        non_jump_successors: list[BoardState] = self.state_peek().get_successor(from_pos, False)
+        non_jump_successors: list[BoardState] = self.state_peek().get_successors_pos_jump(from_pos, False)
 
         for succ in non_jump_successors:
             if succ.get_from_pos() == from_pos and succ.get_to_pos() == to_pos:
-                self.__update_state()
+                self.__update_state(succ)
                 return MoveFeedBack.SUCCESS
 
         if dy > 1:
@@ -55,16 +56,15 @@ class Game:
         return MoveFeedBack.UNKNOWN_INVALID
 
     def move_feedback_click(self) -> MoveFeedBack:
-        jump_successors: list[BoardState] = self.state_peek().get_successors(True)
+        jump_successors: list[BoardState] = self.state_peek().get_successors_jump(True)
         if len(jump_successors) > 0:
             return MoveFeedBack.FORCED_JUMP
         else:
             return MoveFeedBack.PIECE_BLOCKED
 
     def get_valid_moves(self, pos: int):
-        return self.state_peek().get_successors(pos)
-    
-    
+        return self.state_peek().get_successors_pos_jump(pos)
+
     def ai_move(self):
         if not self.is_over() and self.state_peek().get_turn() == Player.AI:
             new_state: BoardState = self.__ai.move(self.state_peek(), Player.AI)
@@ -77,3 +77,30 @@ class Game:
         self.__state.append(new_state)
         if len(self.__state) > self.__memory:
             self.__state.remove(self.__state[0])
+
+    def get_state(self) -> BoardState:
+        return self.state_peek()
+
+    def get_turn(self) -> Player:
+        return self.state_peek().get_turn()
+
+    def is_over(self) -> bool:
+        is_over: bool = self.get_state().is_game_over()
+        if is_over:
+            self.__human_won = self.get_state().piece_count.get(Player.AI) == 0
+        return is_over
+
+    def get_game_over_message(self):
+        result: str = "Game over. "
+        if self.__human_won:
+            result += "YOU WIN"
+        else:
+            result += "YOU LOSE"
+
+        return result
+
+    def undo(self):
+        if len(self.__state) > 2:
+            self.__state.pop()
+            while self.get_state().get_turn() == Player.AI:
+                self.__state.pop()
