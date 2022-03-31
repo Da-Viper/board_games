f66f5ca3c17e2ab400e11946f5f5f59589a86ccb
Ezike Ebuka <yerimyah1@gmail.com>
2022-03-31 16:20:10 +0100

    Add: The game board pieces show up correctly

--- gui/GameBlock.py
+++ gui/GameBlock.py
@@ -1,6 +1,11 @@
+import typing
+
+from PySide2.QtCore import QPointF
 from PySide2.QtGui import QPainter, Qt
 from PySide2.QtWidgets import QGraphicsRectItem, QStyleOptionGraphicsItem, QWidget, QGraphicsItem
 
+from game.piece import Piece
+from game.player import Player
 from game.settings import Settings
 
 
@@ -8,8 +13,7 @@ class BTile(QGraphicsRectItem):
 
     def __init__(self, pos_x, pos_y, parent=None):
         super(BTile, self).__init__(parent)
-        self.setFlag(QGraphicsItem.ItemIsMovable)
-        self.Pressed = False
+
 
         scale = Settings.G_WIDTH
         self.setRect(pos_x * scale, pos_y * scale, scale, scale)
@@ -27,3 +31,34 @@ class BTile(QGraphicsRectItem):
 
         painter.fillRect(self.boundingRect(), self.color)
         # painter.drawRoundRect(-10, -10, constants.T_WIDTH, constants.T_HEIGHT, 5, 5)
+
+
+class GPiece(QGraphicsRectItem):
+
+    def __init__(self, x: int, y: int, pos: int, piece: Piece, parent=None):
+        super(GPiece, self).__init__(parent)
+        self.setFlag(QGraphicsItem.ItemIsMovable)
+        self.Pressed = False
+
+        self.p_pos = pos
+        scale = Settings.G_WIDTH
+        self.setPos(x, y)
+        self.setRect(x * scale, y * scale, scale, scale)
+        self.piece: Piece = piece
+        self.color = Qt.black
+
+        if piece.player is Player.HUMAN:
+            self.setAcceptedMouseButtons(Qt.LeftButton)
+            self.setAcceptTouchEvents(Qt.ToolTip)
+            self.color = Qt.red
+
+    def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: typing.Optional[QWidget]) -> None:
+        # super().paint(painter, option, widget)
+        # x, y = self.boundingRect().x(), self.boundingRect().y()
+        center = self.boundingRect().center()
+        print(f"gpiece_pos {center}")
+        # pos.setX(pos.x() )
+        # pos.setY(pos.y() + 75 / 3)
+        # painter.fillPath()
+        painter.setBrush(self.color)
+        painter.drawEllipse(center, 75 / 3, 75 / 3)
--- gui/gamewindow.py
+++ gui/gamewindow.py
@@ -4,22 +4,32 @@ from PySide2.QtGui import QBrush, Qt, QPen, QPainter, QColor
 from PySide2.QtWidgets import QGraphicsScene, QGraphicsView, QMainWindow, QWidget, QDialog, QGraphicsItemGroup, \
     QGraphicsItem
 
+from game.boardstate import BoardState
+from game.game import Game
 from game.settings import Settings
-from gui.GameBlock import BTile
+from gui.GameBlock import BTile, GPiece
 
 
 class GDialog(QDialog):
 
     def __init__(self, parent=None):
         super(GDialog, self).__init__(parent)
+        ##
+        self.game = Game()
+        self.possible_moves: List[BoardState] = []
+
+        ##  UI
         self.setMinimumSize(Settings.T_WIDTH * Settings.G_WIDTH + 20, Settings.T_HEIGHT * Settings.G_HEIGHT + 20)
-        self.g_group: QGraphicsItemGroup = QGraphicsItemGroup()
+        self.t_group: QGraphicsItemGroup = QGraphicsItemGroup()
+        self.p_group: QGraphicsItemGroup = QGraphicsItemGroup()
         self.tiles: [QGraphicsItem] = []
+        self.pieces: [QGraphicsItem] = []
         self.scene: QGraphicsScene = QGraphicsScene(self)
         self.view: QGraphicsView = QGraphicsView(self.scene, self)
         self.tiles: List[QGraphicsItem] = [None] * Settings.SQUARE_NO
         self.create_scene()
-        self.add_tiles()
+        # self.add_tiles()
+        self.add_pieces()
 
     def create_scene(self):
         scene = self.scene
@@ -27,7 +37,8 @@ class GDialog(QDialog):
         scene.setSceneRect(0, 0, Settings.T_WIDTH * Settings.G_WIDTH, Settings.T_HEIGHT * Settings.G_HEIGHT)
         view.setGeometry(0, 0, Settings.T_WIDTH * Settings.G_WIDTH + 20, Settings.T_HEIGHT * Settings.G_HEIGHT + 20)
 
-        scene.addItem(self.g_group)
+        scene.addItem(self.t_group)
+        scene.addItem(self.p_group)
 
         view.show()
 
@@ -35,9 +46,16 @@ class GDialog(QDialog):
         for i in range(Settings.SQUARE_NO):
             grid_x = i % Settings.T_DIMEN
             grid_y = i // Settings.T_DIMEN
-            self.tiles[i] = BTile(grid_x, grid_y)
-            self.g_group.addToGroup(self.tiles[i])
+            curr_tile = BTile(grid_x, grid_y)
+            self.tiles[i] = curr_tile
+            self.t_group.addToGroup(curr_tile)
 
     def add_pieces(self):
         for i in range(Settings.SQUARE_NO):
-            pass
+            grid_x = i % Settings.T_DIMEN
+            grid_y = i // Settings.T_DIMEN
+            curr_piece = self.game.get_state().get_piece(i)
+            if curr_piece is not None:
+                curr_gpiece = GPiece(grid_x, grid_y, i, curr_piece)
+                self.pieces.append(curr_gpiece)
+                self.p_group.addToGroup(curr_gpiece)
