dced03f5c0a1d49626be2a667965025064b6090f
Ezike Ebuka <yerimyah1@gmail.com>
2022-04-04 14:14:21 +0100

    Fix: Git Ignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Checkers Game',
+    version='',
+    packages=['gui', 'game', 'main'],
+    url='',
+    license='',
+    author='Joseph Semgalawe',
+    author_email='',
+    description='Checkers using Pyside'
+)
