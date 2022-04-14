6ddffca04938768c2e625ec5fb96f3053d76b2a8
Ezike Ebuka <yerimyah1@gmail.com>
2022-04-14 21:33:07 +0100

    Add: Main Menu styles and option to other games

--- gui/mainmenu.py
+++ gui/mainmenu.py
@@ -1,7 +1,9 @@
-from PySide2.QtCore import Slot
-from PySide2.QtWidgets import QMainWindow, QWidget
+from PySide2.QtCore import Slot, QTimer, QObject
+from PySide2.QtWidgets import QWidget
 
 from game.checkers.gui.gamemenu import GameMenu
+from game.sliding_puzzle.gui.puzzlescene import SlidingMenu
+from game.tictactoe.gui.tttmenu import TTTMenu
 from gui.ui_files.ui_mainmenu import Ui_GameMainMenu
 
 
@@ -11,20 +13,22 @@ class MainMenu(QWidget):
         super(MainMenu, self).__init__(parent)
         self.ui = Ui_GameMainMenu()
         self.ui.setupUi(self)
-        self.setMinimumSize(600, 480)
 
         self.ui.btn_checkers.clicked.connect(self.show_checkers)
+        self.ui.btn_ttt.clicked.connect(self.show_tic_tac_toe)
+        self.ui.btn_15_puzzle.clicked.connect(self.show_15_puzzle)
 
     @Slot()
     def show_checkers(self):
-        print("i go here")
         checker_menu = GameMenu(self)
         checker_menu.show()
 
     @Slot()
     def show_tic_tac_toe(self):
-        pass
+        ttt_menu = TTTMenu(self)
+        ttt_menu.show()
 
     @Slot()
     def show_15_puzzle(self):
-        pass
+        puzzle_15 = SlidingMenu()
+        puzzle_15.exec_()
--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import sys
 from PySide2.QtCore import QCoreApplication, Qt
 from PySide2.QtWidgets import QApplication, QStyleFactory
 
-from game.checkers.gui.gamemenu import GameMenu
 from gui.mainmenu import MainMenu
 
 
 def main():
-    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
     pt = "QT_QPA_PLATFORMTHEME"
     # print(os.environ)
@@ -17,6 +16,9 @@ def main():
     print(f"env {os.environ.get(pt)}")
     app.setStyle("Adwaita-Dark")
     print(f"style: {app.style().objectName()}")
+    with open("assets/mine.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
 
     gmenu = MainMenu()
     gmenu.show()
