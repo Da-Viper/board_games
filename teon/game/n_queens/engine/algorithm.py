374d37a2a48164b7fb861f90cd075280cf9d86fe
Ezike Ebuka <yerimyah1@gmail.com>
2022-05-07 21:04:50 +0100

    Add: Sliding Puzzle: Faster Nqueens

--- teon/game/n_queens/engine/algorithm.py
+++ teon/game/n_queens/engine/algorithm.py
@@ -19,6 +19,9 @@ def bit_value(number: int, index: int):
 def get_sol(board: NQueen, all_sol: bool = True):
     if all_sol:
         return _all_solution(board, 0, [])
+        # return new_one(board.queens_pos, board.dimension, 0, board.left_diag,
+        #                board.right_diag, board.visited_col, [])
+
     one_sol = _one_solution(board, 0)
     if one_sol:
         print("One solution ", board.queens_pos)
@@ -27,6 +30,26 @@ def get_sol(board: NQueen, all_sol: bool = True):
         # return another(board.queens_pos, board.dimension, 0, board.visited_row, board.visited_col, board.left_diag,
         #                board.right_diag, board.fixed_row, [])
     # pass
+
+
+def new_one(board, dimen: int, col, ld, rd, cl, sols: List[int]):
+    if col >= dimen:
+        sols.append(board.copy())
+        # return True
+
+    for i in range(dimen):
+
+        if not ((ld[i - col + dimen - 1] or rd[i + col]) or cl[i]):
+            board[i][col] = True
+            ld[i - col + dimen - 1] = rd[i + col] = cl[i] = True
+
+            new_one(board, dimen, col + 1, ld, rd, cl, sols)
+            # return True
+
+            board[i][col] = False  # BACKTRACK
+            ld[i - col + dimen - 1] = rd[i + col] = cl[i] = False
+
+    return sols
 
 
 # def _is_safe(row: int, col: int, v_row: int, v_col: int, v_ldiag: int, v_rdiag: int, offset: int) -> int:
@@ -55,10 +78,10 @@ def _all_solution(nqueen: NQueen, row: int, solutions: List) -> List:
     """
     board = nqueen.queens_pos
     dimension = nqueen.dimension
-    visited_row = nqueen.visited_row
-    visited_col = nqueen.visited_col
-    left_diag = nqueen.left_diag
-    right_diag = nqueen.right_diag
+    v_row = nqueen.visited_row
+    v_col = nqueen.visited_col
+    l_diag = nqueen.left_diag
+    r_diag = nqueen.right_diag
 
     # if at the end add the solution
     if row >= dimension:
@@ -71,15 +94,16 @@ def _all_solution(nqueen: NQueen, row: int, solutions: List) -> List:
 
     for col in range(dimension):
 
+        ldiag, rdiag = col - row, col + row
         # check if there is a queen in the row, column, left diagonal and right diagonal
         # if nqueen._is_safe((row, col)):
-        if _is_safe((row, col), visited_row, visited_col, left_diag, right_diag):
+        # if _is_safe((row, col), v_row, v_col, l_diag, r_diag):
+        if not (v_row[row] or v_col[col] or l_diag[ldiag] or r_diag[rdiag]):
             board[row][col] = Piece.Q_VALUE
-            ldiag, rdiag = col - row, col + row
 
             # set the row, col, left diagonal, right diagonal  as having a queen
-            visited_row[row], visited_col[col] = True, True
-            left_diag[ldiag], right_diag[rdiag] = True, True
+            v_row[row], v_col[col] = True, True
+            l_diag[ldiag], r_diag[rdiag] = True, True
 
             # go to the next row
             _all_solution(nqueen, row + 1, solutions)
@@ -88,8 +112,8 @@ def _all_solution(nqueen: NQueen, row: int, solutions: List) -> List:
             board[row][col] = Piece.EMPTY
 
             # set it back to the default
-            visited_row[row], visited_col[col] = False, False
-            left_diag[ldiag], right_diag[rdiag] = False, False
+            v_row[row], v_col[col] = False, False
+            l_diag[ldiag], r_diag[rdiag] = False, False
 
     return solutions
 
@@ -145,16 +169,8 @@ def another(board, dimen: int, col: int, v_row: int, v_col: int, v_ldiag: int, v
     if col >= dimen:
         solutions.append(copy.deepcopy(board))
         return solutions
-
     if f_row[col]:
         another(board, dimen, col + 1, v_row, v_col, v_ldiag, v_rdiag, f_row, solutions)
-
-    # v_l =
-
-    # mask = 2 ** dimen - 1
-    # v_l = bit_extracted(v_ldiag, dimen, dimen - row)
-    # v_r = bit_extracted(v_rdiag, dimen, row)
-    # current_open = v_r | v_l | v_col
 
     # while current_open & mask:
     for row in range(dimen):
@@ -177,7 +193,7 @@ def another(board, dimen: int, col: int, v_row: int, v_col: int, v_ldiag: int, v
 if __name__ == '__main__':
     board_size = 8
     nqueen_pos = np.empty((board_size, board_size), dtype=np.int8)
-    print(nqueen_pos)
+    # print(nqueen_pos)
 
     nqueen_b = NQueen(nqueen_pos, board_size)
     start = time.perf_counter_ns()
@@ -185,7 +201,7 @@ if __name__ == '__main__':
     end = time.perf_counter_ns()
 
     print(len(res))
-    print(res)
+    # print(res)
     print(f"Total time take is {(end - start) / 1000000} millis ")
 
 # With bool
